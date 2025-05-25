0da0e06f8f07a7fbbdd8ca0a468dd5866beec114
AEMS1 <aems91806@gmail.com>
2025-05-26 03:27:08 +0330

    Add files via upload

---
+++ abi.js
@@ -0,0 +1,27 @@
+const LGD_ABI = [
+    // فقط بخش‌های ضروری ABI برای خواندن موجودی و انتقال
+    {
+      "constant": true,
+      "inputs": [{"name": "_owner", "type": "address"}],
+      "name": "balanceOf",
+      "outputs": [{"name": "balance", "type": "uint256"}],
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [{"name": "", "type": "uint8"}],
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {"name": "_to", "type": "address"},
+        {"name": "_value", "type": "uint256"}
+      ],
+      "name": "transfer",
+      "outputs": [{"name": "", "type": "bool"}],
+      "type": "function"
+    }
+  ];
