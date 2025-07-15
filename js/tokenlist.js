2f7156ab63899d28ecd6fd40725b22db24ec5077
AEMS1 <aems91806@gmail.com>
2025-07-15 19:40:49 +0330

    Delete js/tokenlist.js

--- js/tokenlist.js
+++
@@ -1,7 +0,0 @@
-const tokenList = [
-    { symbol: "BNB", address: null, decimals: 18 },
-    { symbol: "AMS", address: "0x887ada8fe79740b913De549f81014f37e2f8D07a", decimals: 18 },
-    { symbol: "USDT", address: "0x55d398326f99059fF775485246999027B3197955", decimals: 18 },
-    { symbol: "ETH", address: "0x2170Ed0880ac9A755fd29B2688956BD959F933F8", decimals: 18 }
-  ];
-  
