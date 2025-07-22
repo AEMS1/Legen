c440bf1e700763866a4d7e8feec86d53f4aed9d7
AEMS1 <aems91806@gmail.com>
2025-07-22 04:34:19 +0330

    Create quizABI.js

---
+++ quizABI.js
@@ -0,0 +1,30 @@
+const quizContractAddress = "0xa3ea1b32784e702797915d836a063a4836378e5c";
+
+const quizABI = [
+  {
+    "inputs": [{"internalType":"address","name":"_tokenAddress","type":"address"}],
+    "stateMutability":"nonpayable",
+    "type":"constructor"
+  },
+  {
+    "inputs": [{"internalType":"string","name":"videoId","type":"string"}, {"internalType":"string","name":"correctAnswer","type":"string"}],
+    "name":"addQuiz",
+    "outputs": [],
+    "stateMutability":"nonpayable",
+    "type":"function"
+  },
+  {
+    "inputs": [{"internalType":"string","name":"videoId","type":"string"}, {"internalType":"string","name":"newAnswer","type":"string"}],
+    "name":"editQuiz",
+    "outputs": [],
+    "stateMutability":"nonpayable",
+    "type":"function"
+  },
+  {
+    "inputs": [{"internalType":"string","name":"videoId","type":"string"}, {"internalType":"string","name":"userAnswer","type":"string"}],
+    "name":"answerQuiz",
+    "outputs": [],
+    "stateMutability":"nonpayable",
+    "type":"function"
+  }
+];
