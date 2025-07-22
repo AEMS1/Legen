ec34a137ba9cc8418924a578c3edea1ba8cf232a
AEMS1 <aems91806@gmail.com>
2025-07-22 05:20:14 +0330

    Update quizABI.js

--- quizABI.js
+++ quizABI.js
@@ -1,31 +1,29 @@
 const quizContractAddress = "0xa3ea1b32784e702797915d836a063a4836378e5c";
 
 const quizABI = [
-  {
-    "inputs": [{"internalType":"address","name":"_tokenAddress","type":"address"}],
-    "stateMutability":"nonpayable",
-    "type":"constructor"
-  },
-  {
-    "inputs": [{"internalType":"string","name":"videoId","type":"string"}, {"internalType":"string","name":"correctAnswer","type":"string"}],
-    "name":"addQuiz",
-    "outputs": [],
-    "stateMutability":"nonpayable",
-    "type":"function"
-  },
-  {
-    "inputs": [{"internalType":"string","name":"videoId","type":"string"}, {"internalType":"string","name":"newAnswer","type":"string"}],
-    "name":"editQuiz",
-    "outputs": [],
-    "stateMutability":"nonpayable",
-    "type":"function"
-  },
-  {
-    "inputs": [{"internalType":"string","name":"videoId","type":"string"}, {"internalType":"string","name":"userAnswer","type":"string"}],
-    "name":"answerQuiz",
-    "outputs": [],
-    "stateMutability":"nonpayable",
-    "type":"function"
-  }
-];
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
 
