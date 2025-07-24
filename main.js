1152bac31503b7a19a3658ece4476c604dbde205
AEMS1 <aems91806@gmail.com>
2025-07-24 05:18:41 +0330

    Update and rename invite.js to main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const airdrops = [
+  {
+    title: "TokenX Airdrop",
+    image: "images/tokenx.png",
+    description: "100 توکن رایگان TokenX دریافت کنید با ثبت‌نام!",
+    link: "https://tokenx.io/airdrop"
+  },
+  {
+    title: "TokenX Airdrop",
+    image: "images/tokenx.png",
+    description: "100 توکن رایگان TokenX دریافت کنید با ثبت‌نام!",
+    link: "https://tokenx.io/airdrop"
+  },
+  ...
+];
