af77f0176cebb6f7181303e10739cb2f3776623b
AEMS1 <aems91806@gmail.com>
2025-07-24 05:24:29 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const airdrops = [
   {
     title: "TokenX Airdrop",
-    image: "images/tokenx.png",
+    image: "logo.png",
     description: "100 توکن رایگان TokenX دریافت کنید با ثبت‌نام!",
     link: "https://tokenx.io/airdrop"
   },
